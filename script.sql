6878e22b42849cec2d78d57c6cb606a6574097b2
yoinervive100 <viveiguaranureche@gmail.com>
2023-11-26 01:07:15 -0500

    agregando script de base de datos

--- script.sql
+++ script.sql
@@ -1,29 +1,143 @@
-SELECT r.id, r.nombre_raza,count(m.Raza_id) raza FROM mascota m
-RIGHT JOIN raza r
-ON m.Raza_id = r.id 
-GROUP BY r.id, r.nombre_raza;
-select *from vacuna;
 
-/*ejecuute el codigo em msyql para que funcione*/ 
+-- MySQL Workbench Forward Engineering
 
-/*
-UPDATE mascota m
-INNER JOIN raza r
-on m.Raza_id = r.id
-INNER JOIN tipomascota t
-ON m.TipoMascota_id = t.id
-SET  m.nombre = 'yoiner', m.FechaNacimiento = '2023-11-02', r.nombre_raza = 'lobo', t.nombre_mascota = 'pastor'
-WHERE m.id = '1';
-*/
-select m.id, m.nombre, m.FechaNacimiento,r.nombre_raza, t.nombre_mascota from mascota m
-INNER JOIN raza r
-on m.Raza_id = r.id
-INNER JOIN tipomascota t
-ON m.TipoMascota_id = t.id
-ORDER BY m.id ASC;
-select * from mascota;
-select *from rol;
-insert into rol values(1,"jefe");
-alter table raza rename column nombre to nombre_raza;
-alter table tipomascota rename column nombre to nombre_mascota;
-select * from user;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8mb3 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`tipomascota`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`tipomascota` (
+  `id` INT NOT NULL,
+  `nombre_mascota` VARCHAR(150) NULL DEFAULT NULL,
+  `EdadEquivalenteJoven` INT NULL DEFAULT NULL,
+  `EdadEquivalenteAdulto` INT NULL DEFAULT NULL,
+  `EdadAdulto` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`raza`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`raza` (
+  `id` INT NOT NULL,
+  `nombre_raza` VARCHAR(150) NULL DEFAULT NULL,
+  `TipoMascota_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Raza_TipoMascota_idx` (`TipoMascota_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Raza_TipoMascota`
+    FOREIGN KEY (`TipoMascota_id`)
+    REFERENCES `mydb`.`tipomascota` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`rol`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`rol` (
+  `id` INT NOT NULL,
+  `nombre` VARCHAR(150) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `username` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `passwor` VARCHAR(45) NULL DEFAULT NULL,
+  `Role_id` INT NOT NULL,
+  `foto` BLOB NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  INDEX `fk_User_Role1_idx` (`Role_id` ASC) VISIBLE,
+  CONSTRAINT `fk_User_Role1`
+    FOREIGN KEY (`Role_id`)
+    REFERENCES `mydb`.`rol` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`mascota`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`mascota` (
+  `id` INT NOT NULL,
+  `nombre` VARCHAR(150) NULL DEFAULT NULL,
+  `FechaNacimiento` DATE NULL DEFAULT NULL,
+  `foto` LONGBLOB NULL DEFAULT NULL,
+  `User` INT NOT NULL,
+  `TipoMascota_id` INT NOT NULL,
+  `Raza_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Mascota_User1_idx` (`User` ASC) VISIBLE,
+  INDEX `fk_Mascota_TipoMascota1_idx` (`TipoMascota_id` ASC) VISIBLE,
+  INDEX `fk_Mascota_Raza1_idx` (`Raza_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Mascota_Raza1`
+    FOREIGN KEY (`Raza_id`)
+    REFERENCES `mydb`.`raza` (`id`),
+  CONSTRAINT `fk_Mascota_TipoMascota1`
+    FOREIGN KEY (`TipoMascota_id`)
+    REFERENCES `mydb`.`tipomascota` (`id`),
+  CONSTRAINT `fk_Mascota_User1`
+    FOREIGN KEY (`User`)
+    REFERENCES `mydb`.`user` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`vacuna`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`vacuna` (
+  `id` INT NOT NULL,
+  `nombre` VARCHAR(150) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`controlvacuna`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`controlvacuna` (
+  `Mascota_id` INT NOT NULL,
+  `Vacuna_id` INT NOT NULL,
+  `fecha` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`Mascota_id`, `Vacuna_id`),
+  INDEX `fk_Mascota_has_Vacuna_Vacuna1_idx` (`Vacuna_id` ASC) VISIBLE,
+  INDEX `fk_Mascota_has_Vacuna_Mascota1_idx` (`Mascota_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Mascota_has_Vacuna_Mascota1`
+    FOREIGN KEY (`Mascota_id`)
+    REFERENCES `mydb`.`mascota` (`id`),
+  CONSTRAINT `fk_Mascota_has_Vacuna_Vacuna1`
+    FOREIGN KEY (`Vacuna_id`)
+    REFERENCES `mydb`.`vacuna` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
